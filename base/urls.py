b1b7fe46b2377f0cc5d6ff85bba5a7151ffe5fe6
whitebbit <whitebbit.entertainment@gmail.com>
2023-05-08 14:57:55 +0300

    login & logout

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate,TaskDelete
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete, LoginViews
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+    path("login/", LoginViews.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    
     path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
     path("task-create", TaskCreate.as_view(), name="task-create"),
