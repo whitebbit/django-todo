ca6b8da7e728db8d9ddb046c2b3582f07ae8fc59
whitebbit <whitebbit.entertainment@gmail.com>
2023-05-08 14:26:40 +0300

    make fuctions for tasks

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import TaskList
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate,TaskDelete
 
 urlpatterns = [
     path("", TaskList.as_view(), name="tasks"),
-    
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("task-create", TaskCreate.as_view(), name="task-create"),
+    path("task-update/<int:pk>/", TaskUpdate.as_view(), name="task-update"),
+    path("task-delete/<int:pk>/", TaskDelete.as_view(), name="task-delete"),
 ]
